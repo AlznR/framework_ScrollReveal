1f1cdb917b93d8ce5218c9e756edb440b5308ba8
AlznR <168142091+AlznR@users.noreply.github.com>
2024-05-24 19:47:46 -0300

    uploading first version
    index.html
    style.css
    script.js

---
+++ Aulita/script.js
@@ -0,0 +1,7 @@
+window.sr = ScrollReveal({reset:true})
+sr.reveal('.area-1', {duration:2000,rotate:{x:80,y:80,z:0}})
+
+sr.reveal('.area-2', {duration:2000,rotate:{x:0,y:-300,z:0}})
+
+sr.reveal('.area-3', {duration:2000,rotate:{x:20000,y:0,z:0}})
+
